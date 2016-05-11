756aa1e0ed5d3acf02fe9cf07975b893ddef9c0a
Admin <avishayhajbi@gmail.com>
2016-05-12 01:00:58 +0300

    update home

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -2,8 +2,23 @@
 
 angular.module('PlayzApp')
 .controller('homeCtrl', function($scope, $http, $rootScope,DB_queries) {
-	DB_queries.getEvents().then(function(data){
-		$scope.events = data;
-	})
 	console.log("home controller")
+	$scope.tabChanged=function(type){
+		if (type=="today"){
+			//DB_queries.getUpcomingEvents().then(function(data){
+			//	$scope.upcomingEvents=data.events;
+			//})
+		}
+		else if (type=="tomorrow"){
+			//DB_queries.getPastEvents().then(function(data){
+			//	$scope.pastEvents=data.events;
+			//})
+		}
+		else if (type=="negotiable"){
+			//DB_queries.getPastEvents().then(function(data){
+			//	$scope.pastEvents=data.events;
+			//})
+		}
+	}
+	$scope.tabChanged('today');
 });
