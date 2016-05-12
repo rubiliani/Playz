66bae7077c91e9845296981db0191c0c3aad7604
Rubi Liani <Rubi.liani@gmail.com>
2016-05-12 21:25:34 +0300

    try to recover

--- public/js/services/DB_queries.js
+++ public/js/services/DB_queries.js
@@ -3,120 +3,42 @@
  * https://github.com/GoDisco/ngFacebook
  */
 angular.module('PlayzApp.services')
-    .factory('DB_queries', function ($http, $q, $rootScope, $localStorage) {
-        var _getEvents = function () {
+    .factory('DB_queries', function($http, $q, $rootScope, $localStorage){
+        var _getEvents = function(){
             var deferred = $q.defer();
-            $http.get($rootScope.app.domain + 'events/other/' + $rootScope.user.id)
-                .success(function (data) {
-                    console.log("get events success", data)
-                    deferred.resolve(data);
-                }).error(function (err) {
-                console.log("get events err", err)
-                deferred.reject(err);
-            })
-                ['finally'](function () {
+            $http.get($rootScope.app.domain+'events/other/'+$rootScope.user.id)
+                 .success(function(data){
+                     console.log("get events success",data)
+                     deferred.resolve(data);
+                 }).error(function(err){
+                    console.log("get events err",err)
+                    deferred.reject(err);
+                })
+                ['finally'](function() {
 
-            });
+                });
             return deferred.promise;
         }
 
-        var _updateUser = function (user) {
+        var _updateUser = function(user){
             var deferred = $q.defer();
-            $http.post($rootScope.app.domain + 'users/update_user', {user: user})
-                .success(function (data) {
-                    console.log("update user success", data.user)
-                    if (data.newUser) {
-                        data.user.newUser = data.newUser;
+            $http.post($rootScope.app.domain+'users/update_user',{user:user})
+                .success(function(data){
+                    console.log("update user success",data.user)
+                    if (data.newUser){
+                        data.user.newUser=data.newUser;
                     }
                     deferred.resolve(data.user);
-                }).error(function (err) {
-                console.log("update user err", err)
-                deferred.reject(err);
+                }).error(function(err){
+                    console.log("update user err",err)
+                    deferred.reject(err);
             })
-                ['finally'](function () {
-
-            });
-            return deferred.promise;
-        }
-        var _getUpcomingEvents = function () {
-            var deferred = $q.defer();
-            $http.post($rootScope.app.domain + 'events/getUpcomingEvents')
-                .success(function (data) {
-                    console.log("getUpcomingEvents success", data)
-                    deferred.resolve(data);
-                }).error(function (err) {
-                console.log("getUpcomingEvents err", err)
-                deferred.reject(err);
-            })
-                ['finally'](function () {
-
-            });
-            return deferred.promise;
-        }
-        var _getPastEvents = function () {
-            var deferred = $q.defer();
-            $http.post($rootScope.app.domain + 'events/getPastEvents')
-                .success(function (data) {
-                    console.log("getPastEvents success", data)
-                    deferred.resolve(data);
-                }).error(function (err) {
-                console.log("getPastEvents err", err)
-                deferred.reject(err);
-            })
-                ['finally'](function () {
+            ['finally'](function() {
 
             });
             return deferred.promise;
         }
 
-        var _getTodayEvents = function () {
-            var deferred = $q.defer();
-            $http.post($rootScope.app.domain + 'events/getTodayEvents')
-                .success(function (data) {
-                    console.log("getPastEvents success", data)
-                    deferred.resolve(data);
-                }).error(function (err) {
-                console.log("getPastEvents err", err)
-                deferred.reject(err);
-            })
-                ['finally'](function () {
-
-            });
-            return deferred.promise;
-        }
-        var _getTomorrowEvents = function () {
-            var deferred = $q.defer();
-            $http.post($rootScope.app.domain + 'events/getTomorrowEvents')
-                .success(function (data) {
-                    console.log("getPastEvents success", data)
-                    deferred.resolve(data);
-                }).error(function (err) {
-                console.log("getPastEvents err", err)
-                deferred.reject(err);
-            })
-                ['finally'](function () {
-
-            });
-            return deferred.promise;
-        }
-        var _getNegotiableEvents = function () {
-            var deferred = $q.defer();
-            $http.post($rootScope.app.domain + 'events/getNegotiableEvents')
-                .success(function (data) {
-                    console.log("getPastEvents success", data)
-                    deferred.resolve(data);
-                }).error(function (err) {
-                console.log("getPastEvents err", err)
-                deferred.reject(err);
-            })
-                ['finally'](function () {
-
-            });
-            return deferred.promise;
-        }
-
-
-<<<<<<< HEAD
         var _createEvent = function(event){
             var deferred = $q.defer();
             $http.post($rootScope.app.domain+'events/createEvent',{event:event})
@@ -138,15 +60,5 @@ angular.module('PlayzApp.services')
             getEvents:_getEvents,
             updateUser:_updateUser,
             createEvent:_createEvent
-=======
-        return {
-            getEvents: _getEvents,
-            updateUser: _updateUser,
-            getUpcomingEvents: _getUpcomingEvents,
-            getPastEvents: _getPastEvents,
-            getTodayEvents: _getTodayEvents,
-            getTomorrowEvents: _getTomorrowEvents,
-            getNegotiableEvents: _getNegotiableEvents
->>>>>>> a325e38d9892bdbd9ba58f4e6c130c12e22bd3fb
         }
     })
