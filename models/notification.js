cde8d17a45c5bccb1d2d5fa8e123463dd410cd5a
Admin <avishayhajbi@gmail.com>
2016-05-13 18:24:30 +0300

    add notifications

--- models/notification.js
+++ models/notification.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 var notificationSchema = new Schema({
     eventTitle : { type : String, default:''},
+    event:[{type : Schema.Types.ObjectId ,  ref: 'events'}],
     creator:{ type : Schema.Types.ObjectId, ref: 'users'},
     whenDate : { type : String, default:''}
 });
