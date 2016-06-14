127e3a239e23ebf00e3baab4d7f96322faab7966
Rubi Liani <Rubi.liani@gmail.com>
2016-06-14 23:33:05 +0300

    general fixes

--- public/js/controllers/event.js
+++ public/js/controllers/event.js
@@ -30,14 +30,14 @@ angular.module('PlayzApp')
 			$scope.$apply();
 
 			if ($rootScope.user.id == msg.sender.id) return;
-			growl.info("New message for "+msg.event.eventTitle+" event", {title: 'New Unread Message'});
+			growl.info("New chat message in "+msg.event.eventTitle+" event", {title: 'New Unread Message'});
 			// $anchorScroll();
 			// $("<a>").attr({'href':"#"+msg._id}).trigger('click')
 		}
 		else{
 			//if ($rootScope.user.id == msg.sender.id) return;
 			//growl.warning("This adds a warn message", {title: 'Warning!'});
-			growl.info("New message for "+msg.event.eventTitle+" event", {title: 'New Unread Message'});
+			growl.info("New chat message in "+msg.event.eventTitle+" event", {title: 'New Unread Message'});
 			//growl.success("This adds a success message"); //no title here
 			//growl.error("This adds a error message", {title: 'ALERT WE GOT ERROR'});
 		}
