b9191f59cc63fcbe5ec0ea6b1e9c0103587244da
Rubi Liani <Rubi.liani@gmail.com>
2016-07-02 16:55:19 +0300

    admin

--- public/js/controllers/admin.js
+++ public/js/controllers/admin.js
@@ -6,6 +6,30 @@ angular.module('PlayzApp')
 
         $scope.userMarkers = [];
         $scope.eventMarkers = [];
+        $scope.numberOfMales = 0;
+        $scope.numberOfFemales=0;
+
+        $scope.sports=[{name:"Baseball"},{name:"Basketball"},{name:"Boxing"},{name:"Chess"},{name:"Cycling"},{name:"Fishing"},{name:"Football"},
+            {name:"Golf"},{name:"Hockey"},{name:"Ping-Pong"},{name:"Pool"},{name:"Running"},{name:"Soccer"},{name:"Surfing"},{name:"Swimming"}
+            ,{name:"Tennis"},{name:"Volleyball"}];
+
+        $scope.Baseball=0;
+        $scope.Basketball=0;
+        $scope.Boxing=0;
+        $scope.Chess=0;
+        $scope.Cycling=0;
+        $scope.Fishing=0;
+        $scope.Football=0;
+        $scope.Golf=0;
+        $scope.Hockey=0;
+        $scope.PingPong=0;
+        $scope.Pool=0;
+        $scope.Running=0;
+        $scope.Soccer=0;
+        $scope.Surfing=0;
+        $scope.Swimming=0;
+        $scope.Tennis=0;
+        $scope.Volleyball=0;
     
          var self = this;
 
@@ -39,6 +63,100 @@ angular.module('PlayzApp')
           infowindow.close();
         });
 
+        var ctx = document.getElementById("myChart");
+        $scope.eventsChart = new Chart(ctx, {
+           type: 'pie',
+          data: {
+                labels: ["Baseball","Basketball","Boxing","Chess","Cycling","Fishing","Football","Golf","Hockey","Ping-Pong","Pool","Running","Soccer","Surfing","Swimming","Tennis","Volleyball"],
+                datasets: [{
+                    label: '# of events',
+                    data: [0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0],
+                    backgroundColor: [
+                        '#68721b',
+                        '#f76921',
+                        '#a06016',
+                        '#54d6fa',
+                        '#3b3a26',
+                        'rgba(255, 99, 132, 0.2)',
+                        '#701e12',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        '#ec1d23',
+                        'rgba(255, 99, 132, 0.2)',
+                        '#1d2b58',
+                        '#4a9a45',
+                        '#d8e20f',
+                        '#4a9a45',
+                        '#d8e20f',
+                        '#1144c4'
+                    ],
+                    borderColor: [
+                         '#68721b',
+                        '#f76921',
+                        '#a06016',
+                        '#54d6fa',
+                        '#3b3a26',
+                        'rgba(255, 99, 132, 0.2)',
+                        '#701e12',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        '#ec1d23',
+                        'rgba(255, 99, 132, 0.2)',
+                        '#1d2b58',
+                        '#4a9a45',
+                        '#d8e20f',
+                        '#4a9a45',
+                        '#d8e20f',
+                        '#1144c4'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+
+        var ctx3 = document.getElementById("myChart3");
+        $scope.usersChart = new Chart(ctx3, {
+         type: 'bar',
+          data: {
+                labels: ["Female", "Male"],
+                datasets: [{
+                    label: '# of Users',
+                    data: [0, 0],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+
+
+       
+
+
         //$scope.markers = [];
         var createEventMarker = function(info) {
          
@@ -46,8 +164,8 @@ angular.module('PlayzApp')
             url: "assets/img/"+info.sportType+".png", //assets/img/{{card.sportType}}.png
             scaledSize : new google.maps.Size(42, 42)
           };
-          var newLat = info.location.latitude + (Math.random() -.5) / 3500;// * (Math.random() * (max - min) + min);
-          var newLng = info.location.longitude + (Math.random() -.5) / 3500;// * (Math.random() * (max - min) + min);
+          var newLat = info.location.latitude + (Math.random() -.5) / 5000;// * (Math.random() * (max - min) + min);
+          var newLng = info.location.longitude + (Math.random() -.5) / 5000;// * (Math.random() * (max - min) + min);
           var marker = new google.maps.Marker({
            // map: self.map,
 
@@ -82,8 +200,8 @@ angular.module('PlayzApp')
           };
         }
 
-          var newLat = info.hometown.latitude + (Math.random() -.5) / 4500;// * (Math.random() * (max - min) + min);
-          var newLng = info.hometown.longitude + (Math.random() -.5) / 4500;// * (Math.random() * (max - min) + min);
+          var newLat = info.hometown.latitude + (Math.random() -.5) / 5000;// * (Math.random() * (max - min) + min);
+          var newLng = info.hometown.longitude + (Math.random() -.5) / 5000;// * (Math.random() * (max - min) + min);
        
           var marker = new google.maps.Marker({
             //map: self.map,
@@ -114,6 +232,7 @@ angular.module('PlayzApp')
                     });*/
                 });
                 $scope.showEvents();
+                 $scope.calculateEvents();
                // console.log( $scope.mymap.points)
             });  
 
@@ -128,6 +247,7 @@ angular.module('PlayzApp')
                         body: event.eventTitle
                     });*/
                 });
+                $scope.calculateUsers();
                // console.log( $scope.mymap.points)
             });  
 
@@ -165,11 +285,104 @@ angular.module('PlayzApp')
           });
       }
 
+      $scope.calculateUsers =function(){
+        $scope.users.forEach(function (user, i) {
+            if(user.gender=="male")
+               $scope.numberOfMales++;
+            else
+               $scope.numberOfFemales++;
+         });
+      }
+
+      $scope.calculateEvents = function(){
+         
+         $scope.events.forEach(function (events, i) {
+          switch(events.sportType){
+            case "Baseball":
+                $scope.Baseball++;
+                break;
+            case "Basketball":
+                $scope.Basketball++;
+                break;
+            case "Boxing":
+                $scope.Boxing++;;
+                break;
+            case "Chess":
+                $scope.Chess++;;
+                break;
+            case "Cycling":
+                $scope.Cycling++;;
+                break;
+            case "Fishing":
+                $scope.Fishing++;;
+                break;
+            case "Football":
+                $scope.Football++;;
+                break;
+            case "Golf":
+                $scope.Golf++;;
+                break;
+            case "Hockey":
+                $scope.Hockey++;;
+                break;
+            case "Ping-Pong":
+                $scope.PingPong++;;
+                break;
+            case "Pool":
+                $scope.Pool++;;
+                break;
+            case "Running":
+                $scope.Running++;;
+                break;
+            case "Soccer":
+                $scope.Soccer++;;
+                break;
+            case "Surfing":
+                $scope.Surfing++;;
+                break;
+            case "Swimming":
+                $scope.Swimming++;;
+                break;
+            case "Tennis":
+                $scope.Tennis++;;
+                break;
+            case "Volleyball":
+                $scope.Volleyball++;;
+                break;
+              }
+          });
+
+         $scope.usersChart.data.datasets[0].data[0] =  $scope.numberOfFemales;
+         $scope.usersChart.data.datasets[0].data[1] =  $scope.numberOfMales;
+
+
+         $scope.eventsChart.data.datasets[0].data[0] =  $scope.Baseball;
+         $scope.eventsChart.data.datasets[0].data[1] =  $scope.Basketball;
+         $scope.eventsChart.data.datasets[0].data[2] =  $scope.Boxing;
+         $scope.eventsChart.data.datasets[0].data[3] =  $scope.Chess;
+         $scope.eventsChart.data.datasets[0].data[4] =  $scope.Cycling;
+         $scope.eventsChart.data.datasets[0].data[5] =  $scope.Fishing;
+         $scope.eventsChart.data.datasets[0].data[6] =  $scope.Football;
+         $scope.eventsChart.data.datasets[0].data[7] =  $scope.Golf;
+         $scope.eventsChart.data.datasets[0].data[8] =  $scope.Hockey;
+         $scope.eventsChart.data.datasets[0].data[9] =  $scope.PingPong;
+         $scope.eventsChart.data.datasets[0].data[10] =  $scope.Pool;
+         $scope.eventsChart.data.datasets[0].data[11] =  $scope.Running;
+         $scope.eventsChart.data.datasets[0].data[12] =  $scope.Soccer;
+         $scope.eventsChart.data.datasets[0].data[13] =  $scope.Surfing;
+         $scope.eventsChart.data.datasets[0].data[14] =  $scope.Swimming;
+         $scope.eventsChart.data.datasets[0].data[15] =  $scope.Tennis;
+         $scope.eventsChart.data.datasets[0].data[16] =  $scope.Volleyball;
+        
+
+         $scope.usersChart.update();
+         $scope.eventsChart.update()
+      }
+
 
         initialize();
       $scope.updateMap();
 
-     
 
     });
 
